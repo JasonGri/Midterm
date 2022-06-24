7960d99d2162ab95a42b1d934c1370ad5ede30c2
JasonGri <Iason.Grivas@acg.edu>
2022-06-24 16:13:34 +0300

    Implemented Adding and validation functionality of the form

--- script.js
+++ script.js
@@ -1,16 +1,79 @@
 'use strict';
 
 // Changes the src path of the svg icon 
-const sortUp = './imgs/sort-up.svg'
-const sortDown = './imgs/sort-down.svg'
-
-
 function toggleSort(){
+    const sortUp = './imgs/sort-up.svg'
+    const sortDown = './imgs/sort-down.svg'
     let sortImg = this.firstChild
     sortImg.src === 'file:///C:/Users/jason/OneDrive/Desktop/Midterm/Midterm/imgs/sort-down.svg' ? sortImg.src = sortUp : sortImg.src = sortDown;
 }
 
+// Validates Input of the form, displays a message in case of error
+function formValidation2(title, rating, comment){
+    // Clear error messages
+    title.parentElement.querySelector('span').innerHTML = '';
+    rating.parentElement.querySelector('span').innerHTML = '';
+    comment.parentElement.querySelector('span').innerHTML = '';
+    let isValid = true;
 
-document.getElementById('title-toggle').addEventListener("click", toggleSort)
-document.getElementById('rating-toggle').addEventListener("click", toggleSort)
+    if (title.value === ''){
+        title.parentElement.querySelector('span').innerHTML = 'Enter a game title!';
+        isValid=false;
+    }
+    if (rating.value === ''){
+        rating.parentElement.querySelector('span').innerHTML = 'Enter a rating!';
+        isValid=false;
+    }
+    if (comment.value === ''){
+        comment.parentElement.querySelector('span').innerHTML = 'Enter a comment!';
+        isValid=false;
+    }
+    if (!(rating.value >= 0 && rating.value <=10 )){
+        rating.parentElement.querySelector('span').innerHTML = 'The rating has to be from 0 - 10!';
+        isValid=false;
+    }
+    return isValid;
+}
 
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    // Toggling Sorting Icon 
+    document.getElementById('title-toggle').onclick = toggleSort;
+    document.getElementById('rating-toggle').onclick = toggleSort;
+
+    // Submission of the form
+    document.querySelector('form').onsubmit = () => {
+        let title = document.getElementById('title');
+        let rating = document.getElementById('rating');
+        let comment = document.getElementById('comment');
+
+        // Stop code execution if input is invalid
+        if (formValidation2(title,rating,comment)===false){
+            return false;
+        }
+
+        // Create new table row
+        const tr = document.createElement('tr');
+        // Customize the format of the tr html code
+        tr.innerHTML =
+        `<th scope="row">2</th>
+        <td>${title.value}</td>
+        <td>${rating.value}</td>
+        <td>${comment.value}</td>
+        <td id="td-dlt">
+        <button type="button" class="btn btn-danger btn-sm"><img class="filter-white" src="./imgs/trash.svg" alt="trashcan" width="30" height="24"></button>
+        </td>`;
+        // Add the row to the table body
+        document.querySelector('tbody').appendChild(tr);
+        // Clear form fields
+        title.value = '';
+        rating.value = '';
+        comment.value = '';
+        
+        // Stop form from submitting
+        return false;
+    };
+
+});
